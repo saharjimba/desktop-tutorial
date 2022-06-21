589492827de414913c512da0d76efd3f045e8b85
saharjimba <aliabbasisahar7@gmail.com>
2022-06-21 16:39:20 +0430

    practice

--- javascript/main2.js
+++ javascript/main2.js
@@ -5,4 +5,21 @@ var PersonAge =27;
 console.log (PersonName);
 console.log (PersonAge);
 */
-console.log (`name: ${PersonName} age: ${PersonAge}`);
+/*console.log (`name: ${PersonName} age: ${PersonAge}`);*/
+
+var Namber =27
+var Name   ="sahar"
+if (Namber < 27 ) {
+    console.log(Name + "is younger");
+    }
+  else if(Namber == 27){
+            console.log(`${Name} is 27 years old.`)
+        }
+    else 
+    {
+        console.log(`${Name} is older.`);
+
+      
+    }
+  
+
