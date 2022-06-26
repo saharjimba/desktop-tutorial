a596bf5b02dca8e5805a191db7cb2791bb4d7f4e
saharjimba <aliabbasisahar7@gmail.com>
2022-06-26 17:59:24 +0430

    arraye

--- javascript/main2.js
+++ javascript/main2.js
@@ -30,30 +30,30 @@ if (Namber < 27 ) {
     console.log (output);
 
     function sum1(num3,num4){
-        var res;
-        res= num3 + num4
-        return res; 
+        //var res;
+        //res= num3 + num4
+   //     return res; 
     }
-    var output= sum1(7,8)
-        console.log (output)
+  //  var output= sum1(7,8)
+      //  console.log (output)
 
-        function send () {
-            var income, outgo;
-          income = document.getElementById("income").value;
-          outgo = document.getElementById("outgo").value;
-          if (Number(income) > Number(outgo)) {
-            console.log ("income > outgo");
-          }
-        else if (Number(income) < Number(outgo)) {
-            console.log ("income < outgo");
-        }
-        else {
-            console.log ("income = outgo");
-        }
+       // function send () {
+         //   var income, outgo;
+         // income = document.getElementById("income").value;
+         // outgo = document.getElementById("outgo").value;
+         // if (Number(income) > Number(outgo)) {
+       //     console.log ("income > outgo");
+        //  }
+       // else if (Number(income) < Number(outgo)) {
+           // console.log ("income < outgo");
+      //  }
+       // else {
+           // console.log ("income = outgo");
+      //  }
         
-        }
+        //}
  
-        var profileInfo = ["Sahar",28,true,"aliabbasisaha7@gmail.com" ]
+        //var profileInfo = ["Sahar",28,true,"aliabbasisaha7@gmail.com" ]
        
         //profileInfo.push("username");
         //console.log(profileInfo);
@@ -61,4 +61,31 @@ if (Namber < 27 ) {
         //profileInfo.pop();
        //var show = profileInfo.indexOf(28)
        //profileInfo[3]= "sahar aliabbasi"
-        console.log (profileInfo);
+        //console.log (profileInfo);
+
+        //var profileInfo = {
+          //  Name: "Sara",
+          //  Email: "sara@gmail.com",
+        //  age: 20,
+          //  job:"music"
+      //  }
+      //  profileInfo ["job"] = "music"
+      //  profileInfo["Name"]="sahar"
+      //  var show = profileInfo["job"];
+       // console.log(show);
+        //console.log(profileInfo);
+        
+ var profileInfo = ["Sahar" , "sahar.aliabbasi@gmail.com" , 27 , "designer"];
+// profileInfo[1]="conect us";
+//profileInfo.pop();
+//profileInfo.shift("sahar");
+//profileInfo.unshift(27);
+profileInfo.push("sahar");
+ console.log(profileInfo);
+
+ var profileInfo = {
+   Name: "ravi",
+   emnail:"ravi.ojha@gmail.com",
+   age:28,
+   job: "SEO wordpress designer",}
+  console.log(profileInfo);
